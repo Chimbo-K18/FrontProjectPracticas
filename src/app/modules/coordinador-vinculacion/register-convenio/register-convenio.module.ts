328b2e1ffd9b8223f47d91b5af5a2869b5e5294f
David <100893416+davicho896@users.noreply.github.com>
2023-04-27 22:16:13 -0500

    Ahi ta esa cosa
    Ahi ta esa cosa

--- src/app/modules/coordinador-vinculacion/register-convenio/register-convenio.module.ts
+++ src/app/modules/coordinador-vinculacion/register-convenio/register-convenio.module.ts
@@ -5,12 +5,27 @@ import { RegisterConvenioRoutingModule } from './register-convenio-routing.modul
 import { RegisterConvenioComponent } from './pages/register-convenio/register-convenio.component';
 
 
+//Importaciones clave
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     RegisterConvenioComponent
   ],
   imports: [
     CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     RegisterConvenioRoutingModule
   ],
   exports:[
