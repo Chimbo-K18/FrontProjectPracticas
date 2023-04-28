9459d9376ba1c81fbc3645242a03fb00e585e1ee
David <100893416+davicho896@users.noreply.github.com>
2023-04-27 23:31:58 -0500

    Revert "Correciones de validaciones"
    This reverts commit 664810e16a421e2bba81aa6cef42909df65527e0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,6 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
-import {MatStepperModule} from '@angular/material/stepper';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
-
 @NgModule({
   declarations: [
     AppComponent
@@ -25,18 +14,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-
-
-    MatStepperModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-
-
-
     RouterModule,
     BrowserAnimationsModule
   ],
--- src/app/modules/coordinador-vinculacion/register-convenio/register-convenio.module.ts
+++ src/app/modules/coordinador-vinculacion/register-convenio/register-convenio.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RegisterConvenioRoutingModule } from './register-convenio-routing.module';
 import { RegisterConvenioComponent } from './pages/register-convenio/register-convenio.component';
 
+
 //Importaciones clave
 import {MatStepperModule} from '@angular/material/stepper';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -19,14 +20,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   ],
   imports: [
     CommonModule,
-
     MatStepperModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
     ReactiveFormsModule,
     MatButtonModule,
-
     RegisterConvenioRoutingModule
   ],
   exports:[
