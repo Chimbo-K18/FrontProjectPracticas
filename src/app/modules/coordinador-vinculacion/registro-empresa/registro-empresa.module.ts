25bc72e453863bd82fc38a866f457eec1cdbc371
David <100893416+davicho896@users.noreply.github.com>
2023-04-28 00:46:21 -0500

    Register-empresa
    Register-empresa

--- src/app/modules/coordinador-vinculacion/registro-empresa/registro-empresa.module.ts
+++ src/app/modules/coordinador-vinculacion/registro-empresa/registro-empresa.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RegistroEmpresaRoutingModule } from './registro-empresa-routing.module';
 import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empresa.component';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,6 +15,10 @@ import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empr
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
     RegistroEmpresaRoutingModule
   ]
 })
