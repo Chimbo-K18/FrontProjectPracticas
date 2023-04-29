d63dccbd972d884b8801ce34d6f6a7edaeb55440
David <100893416+davicho896@users.noreply.github.com>
2023-04-28 19:13:24 -0500

    LANZAMIENTO DE CONVOCATORIA
    LANZAMIENTO DE CONVOCATORIA

--- src/app/modules/responsable-ppp/lanzamiento-convocatoria/lanzamiento-convocatoria.module.ts
+++ src/app/modules/responsable-ppp/lanzamiento-convocatoria/lanzamiento-convocatoria.module.ts
@@ -5,12 +5,37 @@ import { LanzamientoConvocatoriaRoutingModule } from './lanzamiento-convocatoria
 import { LanzamientoConvocatoriaComponent } from './pages/lanzamiento-convocatoria/lanzamiento-convocatoria.component';
 
 
+//Importaciones clave
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+
 @NgModule({
   declarations: [
     LanzamientoConvocatoriaComponent
   ],
   imports: [
     CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatDatepickerModule,
     LanzamientoConvocatoriaRoutingModule
   ],
   exports:[
