cb91406a038cb0bdded86a19ccae456ecaa0d293
ChristianBarb01 <christianp.barbecho.est@tecazuay.edu.ec>
2023-05-01 05:09:32 -0500

    solis enviadas

--- src/app/modules/tutor-empresarial/lista-soli-enviadas/lista-soli-enviadas.module.ts
+++ src/app/modules/tutor-empresarial/lista-soli-enviadas/lista-soli-enviadas.module.ts
@@ -3,6 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ListaSoliEnviadasRoutingModule } from './lista-soli-enviadas-routing.module';
 import { ListaSoliEnviadasComponent } from './pages/lista-soli-enviadas/lista-soli-enviadas.component';
+//Importaciones clave
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +23,17 @@ import { ListaSoliEnviadasComponent } from './pages/lista-soli-enviadas/lista-so
   ],
   imports: [
     CommonModule,
-    ListaSoliEnviadasRoutingModule
+    ListaSoliEnviadasRoutingModule,
+    CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatIconModule
   ],
   exports:[
     ListaSoliEnviadasComponent
--- src/app/modules/tutor-empresarial/lista-soli-enviadas/pages/lista-soli-enviadas/lista-soli-enviadas.component.ts
+++ src/app/modules/tutor-empresarial/lista-soli-enviadas/pages/lista-soli-enviadas/lista-soli-enviadas.component.ts
@@ -1,15 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 'solicitud tds', name: '29/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '20/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '12/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '10/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '09/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '06/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '04/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '03/05/2023', weight: 'Aplica', symbol: 'H'},
+  {position: 'solicitud tds', name: '02/05/2023', weight: 'Aplica', symbol: 'H'},
+];
+//////////seguna tabla
+export interface solicitudes {
+  name: string;
+  fecha: string;
+  carrera: string;
+  estado: string;
+}
+const ELEMENT_DATA2: solicitudes[] = [
+  {name: 'tamara cubillos', fecha: '29/05/2023', carrera: 'Desarrollo Software', estado: 'H'},
+  {name: 'veronica naula', fecha: '29/05/2023', carrera: 'Desarrollo Software', estado: 'H'},
+  {name: 'marlene peralta', fecha: '29/05/2023', carrera: 'Desarrollo Software', estado: 'H'},
+  {name: 'priscila sanchez', fecha: '29/05/2023', carrera: 'Desarrollo Software', estado: 'H'},
+
+];
+///tercera tabla
+export interface asignarHorario {
+  name: string;
+  jornada: string;
+  carrera: string;
+  horario: string;
+  asignar: string;
+}
+const ELEMENT_DATA3: asignarHorario[] = [
+  {name: 'tamara cubillos', jornada: 'matutina', carrera: 'TDS', horario: '07:00-18:00', asignar: 'ok'},
+  {name: 'veronica naula', jornada: 'matutina', carrera: 'TDS', horario: '07:00-18:00', asignar: 'ok'},
+  {name: 'marlene peralta', jornada: 'matutina', carrera: 'TDS', horario: '07:00-18:00', asignar: 'ok'},
+  {name: 'priscila sanchez', jornada: 'matutina', carrera: 'TDS', horario: '07:00-18:00', asignar: 'ok'},
+
+];
 @Component({
   selector: 'app-lista-soli-enviadas',
   templateUrl: './lista-soli-enviadas.component.html',
   styleUrls: ['./lista-soli-enviadas.component.css']
 })
-export class ListaSoliEnviadasComponent implements OnInit {
+export class ListaSoliEnviadasComponent {
+//TABLA
+displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+//segunda tabla
+displayedColumns2: string[] = ['name', 'fecha', 'carrera', 'estado'];
+dataSource2 = new MatTableDataSource<solicitudes>(ELEMENT_DATA2);
+//tercera tabla
+displayedColumns3: string[] = ['name', 'jornada', 'carrera', 'horario', 'asignar'];
+dataSource3 = new MatTableDataSource<asignarHorario>(ELEMENT_DATA3);
 
-  constructor() { }
+@ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
-  }
+ngAfterViewInit() {
+  this.dataSource.paginator = this.paginator;
+}
+
+//FINTABLA
+
+
+
+
+
+firstFormGroup = this._formBuilder.group({
+  firstCtrl: ['', Validators.required],
+});
+secondFormGroup = this._formBuilder.group({
+  secondCtrl: [''],
+});
+
+isEditable = false;
+
+constructor(private _formBuilder: FormBuilder) {}
+
+ngOnInit(): void {
+}
 
 }
