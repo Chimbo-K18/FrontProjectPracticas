60245e560d3192780530e8ca460a154c0bb814f3
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-05-02 17:39:33 -0500

    guardar empresa
    guardar empresa

--- src/app/modules/coordinador-vinculacion/registro-empresa/pages/registro-empresa/registro-empresa.component.ts
+++ src/app/modules/coordinador-vinculacion/registro-empresa/pages/registro-empresa/registro-empresa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from 'src/app/models/empresa';
+import { EmpresaService } from 'src/app/services/empresa.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro-empresa',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-empresa.component.css']
 })
 export class RegistroEmpresaComponent implements OnInit {
-
-  constructor() { }
+  empresa: Empresa = new Empresa();
+  constructor(private empresaServices: EmpresaService) { }
 
   ngOnInit(): void {
+  }
+
+  crearEmpresa(){
+    this.empresaServices.crearEmpresa(this.empresa)
+    .subscribe(response => console.log('Exito al Registrar la empresa'));
+    Swal.fire({
+      position: 'top',
+      icon: 'success',
+      title: 'Empresa creada satisfactoriamente.',
+      showConfirmButton: false,
+      timer: 2000,
+    });
   }
 
 }
