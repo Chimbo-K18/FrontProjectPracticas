70d3e6fd9d3433bcde54410b69bb797543582ce5
CarlosI01 <91437492+CarlosI01@users.noreply.github.com>
2023-05-02 04:48:38 -0500

    cambios
    cambios

--- src/app/modules/coordinador-vinculacion/registro-empresa/pages/registro-empresa/registro-empresa.component.ts
+++ src/app/modules/coordinador-vinculacion/registro-empresa/pages/registro-empresa/registro-empresa.component.ts
@@ -13,10 +13,38 @@ export class RegistroEmpresaComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   crearEmpresa(){
     this.empresaServices.crearEmpresa(this.empresa)
     .subscribe(response => console.log('Exito al Registrar la empresa'));
+  }
+
+  //Metodo Para limpiar los Campos
+  guardado = false;
+  Limpiar(){
+    this.empresa.rucEmpresa = ''; // Limpiar el campo "Ruc"
+    this.empresa.nombreEmpresa = ''; // Limpiar el campo "NombreEmpresa"
+    this.empresa.correo = ''; // Limpiar el campo "Correo"
+    this.empresa.ciudad = ''; // Limpiar el campo "Ciudad"
+    this.empresa.numeroTelefono = ''; // Limpiar el campo "Telefono"
+    this.empresa.direccion = ''; // Limpiar el campo "Direccion"
+    this.empresa.codigoPostal = ''; // Limpiar el campo "CodigoPostal"
+    this.empresa.descripcion = ''; // Limpiar el campo "Descripcion"
+    this.guardado = true;
+  }
+  //Solo Registra Numeros
+  soloNumeros(event: KeyboardEvent) {
+    const codigo = event.keyCode;
+    if ((codigo < 48 || codigo > 57) && codigo !== 8) {
+      event.preventDefault();
+    }
+  }
+  //Solo Texto
+  soloTexto(event: KeyboardEvent) {
+    const codigo = event.keyCode;
+    if ((codigo >= 48 && codigo <= 57) || (codigo >= 96 && codigo <= 105)) {
+      event.preventDefault();
+    }
   }
 
 }
