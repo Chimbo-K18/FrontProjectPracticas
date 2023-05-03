de7f0c250e858f96ab5abe0bf4fcd968729956a4
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2023-05-02 20:12:44 -0500

    Vista de solicitud de practicas

--- src/app/modules/tutor-empresarial/envio-solicitud/envio-solicitud.module.ts
+++ src/app/modules/tutor-empresarial/envio-solicitud/envio-solicitud.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { EnvioSolicitudRoutingModule } from './envio-solicitud-routing.module';
 import { EnvioSolicitudComponent } from './pages/envio-solicitud/envio-solicitud.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +18,15 @@ import { EnvioSolicitudComponent } from './pages/envio-solicitud/envio-solicitud
   ],
   imports: [
     CommonModule,
-    EnvioSolicitudRoutingModule
+    MatStepperModule,
+    EnvioSolicitudRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
   ],
   exports: [
     EnvioSolicitudComponent
--- src/app/modules/tutor-empresarial/envio-solicitud/pages/envio-solicitud/envio-solicitud.component.ts
+++ src/app/modules/tutor-empresarial/envio-solicitud/pages/envio-solicitud/envio-solicitud.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+
+
 @Component({
   selector: 'app-envio-solicitud',
   templateUrl: './envio-solicitud.component.html',
-  styleUrls: ['./envio-solicitud.component.css']
+  styleUrls: ['./envio-solicitud.component.css'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {showError: true},
+    },
+  ],
 })
 export class EnvioSolicitudComponent implements OnInit {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
-  constructor() { }
+  thirdFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     const dropArea = document.querySelector<HTMLElement>(".drop_box")!;
