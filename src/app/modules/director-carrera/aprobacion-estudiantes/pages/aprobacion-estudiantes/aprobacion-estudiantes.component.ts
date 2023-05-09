df3fbe5b40199a1be262a0ea4bff7fa8218fa6dd
ChristianBarb01 <christianp.barbecho.est@tecazuay.edu.ec>
2023-05-09 18:52:12 -0500

    ultimo cambio

--- src/app/modules/director-carrera/aprobacion-estudiantes/pages/aprobacion-estudiantes/aprobacion-estudiantes.component.ts
+++ src/app/modules/director-carrera/aprobacion-estudiantes/pages/aprobacion-estudiantes/aprobacion-estudiantes.component.ts
@@ -12,126 +12,6 @@ import { EstudiantePracticanteService } from 'src/app/services/estudiantepractic
 import Swal from 'sweetalert2';
 import { MatStepper } from '@angular/material/stepper';
 
-export interface PeriodicElement {
-  name: string;
-  position: string;
-  weight: string;
-  estado: string;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-001',
-    name: '05-01-2022',
-    weight: '10-01-2022',
-    estado: 'Finalizado',
-    symbol: 'H',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-002',
-    name: '20-01-2022',
-    weight: '25-01-2022',
-    estado: 'Finalizado',
-    symbol: 'He',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-003',
-    name: '08-02-2022',
-    weight: '13-02-2022',
-    estado: 'Finalizado',
-    symbol: 'Li',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-004',
-    name: '23-02-2022',
-    weight: '28-02-2022',
-    estado: 'Finalizado',
-    symbol: 'Be',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-005',
-    name: '13-03-2022',
-    weight: '18-03-2022',
-    estado: 'Finalizado',
-    symbol: 'B',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-006',
-    name: '28-03-2022',
-    weight: '02-04-2022',
-    estado: 'Finalizado',
-    symbol: 'C',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-007',
-    name: '22-04-2022',
-    weight: '27-04-2022',
-    estado: 'Finalizado',
-    symbol: 'N',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-008',
-    name: '05-05-2022',
-    weight: '10-05-2022',
-    estado: 'Finalizado',
-    symbol: 'O',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-009',
-    name: '20-05-2022',
-    weight: '25-05-2022',
-    estado: 'Finalizado',
-    symbol: 'F',
-  },
-  {
-    position: 'CONVOCATORIA – TSDS -PPP-2022-010',
-    name: '10-06-2022',
-    weight: '15-06-2022',
-    estado: 'Finalizado',
-    symbol: 'Ne',
-  },
-];
-
-export interface Aprobados {
-  nombre: string;
-  fecha: string;
-  carrera: string;
-  esta: string;
-}
-
-const AP: Aprobados[] = [
-  {
-    nombre: 'Bryam Tenecota',
-    fecha: '05-01-2022',
-    carrera: 'TDS',
-    esta: 'Finalizado',
-  },
-  {
-    nombre: 'Carlos Ibarra',
-    fecha: '05-01-2022',
-    carrera: 'TDS',
-    esta: 'Finalizado',
-  },
-  {
-    nombre: 'Christian Barbecho',
-    fecha: '05-01-2022',
-    carrera: 'TDS',
-    esta: 'Finalizado',
-  },
-  {
-    nombre: 'Erika Fernandez',
-    fecha: '08-01-2022',
-    carrera: 'TDS',
-    esta: 'Finalizado',
-  },
-  {
-    nombre: 'Adriana Jaya',
-    fecha: '08-01-2022',
-    carrera: 'TDS',
-    esta: 'Finalizado',
-  },
-];
 
 @Component({
   selector: 'app-aprobacion-estudiantes',
@@ -140,9 +20,6 @@ const AP: Aprobados[] = [
 })
 export class AprobacionEstudiantesComponent implements AfterViewInit {
 SolicitudConvocatoria: SolicitudConvocatoria= new SolicitudConvocatoria();
-
-
-
 
   //TABLA1
   //TABLA convocatorias
@@ -158,20 +35,16 @@ SolicitudConvocatoria: SolicitudConvocatoria= new SolicitudConvocatoria();
   loading: boolean = true;
   dataSource = new MatTableDataSource<Convocatorias>([]);
   //TABLA2
-loadingest:boolean=true;
 datosCargados: boolean = false;
 datosTabla: any[] = [];
-
   // tabla3
   datosCargadosAprobados: boolean = false;
 datosTablaAprobados: any[] = [];
 
 
-  diColumns: string[] = ['nombre', 'fecha', 'carrera', 'esta'];
-  datam = new MatTableDataSource<Aprobados>(AP);
-
   @ViewChild('paginator1', { static: true }) paginator1!: MatPaginator;
   @ViewChild('paginator2', { static: true }) paginator2!: MatPaginator;
+  @ViewChild('paginator3', { static: true }) paginator3!: MatPaginator;
   @ViewChild(MatStepper) stepper!: MatStepper;
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator1;
