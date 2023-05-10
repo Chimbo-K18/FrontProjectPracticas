617ce05e58d03b11a3151785fcde6c8f0c2d6e15
David <100893416+davicho896@users.noreply.github.com>
2023-05-09 22:04:53 -0500

    responsable/asignar-academico (Asignar Tutores)
    responsable/asignar-academico(Asignar Tutores)

--- src/app/modules/responsable-ppp/asigna-academico/asigna-academico.module.ts
+++ src/app/modules/responsable-ppp/asigna-academico/asigna-academico.module.ts
@@ -4,6 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AsignaAcademicoRoutingModule } from './asigna-academico-routing.module';
 import { AsignaAcademicoComponent } from './pages/asigna-academico/asigna-academico.component';
 
+//Importaciones clave
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -11,6 +25,17 @@ import { AsignaAcademicoComponent } from './pages/asigna-academico/asigna-academ
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
     AsignaAcademicoRoutingModule
   ],
   exports:[
