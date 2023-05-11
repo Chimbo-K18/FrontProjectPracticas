1fdc9291ca042daee8a8d0c352d5f8a9c6e8aafe
David <100893416+davicho896@users.noreply.github.com>
2023-05-11 02:36:47 -0500

    empresarial/asigna-tutor
    empresarial/asigna-tutor

--- src/app/modules/tutor-empresarial/asigna-especifico/asigna-especifico.module.ts
+++ src/app/modules/tutor-empresarial/asigna-especifico/asigna-especifico.module.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AsignaEspecificoRoutingModule } from './asigna-especifico-routing.module';
 import { AsignaEspecificoComponent } from './pages/asigna-especifico/asigna-especifico.component';
+//Importaciones clave
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -11,6 +24,17 @@ import { AsignaEspecificoComponent } from './pages/asigna-especifico/asigna-espe
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
     AsignaEspecificoRoutingModule
   ],
   exports:[
