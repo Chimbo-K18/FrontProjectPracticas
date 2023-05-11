7727d5e1dc6a8a08134471b5d66d3100bec21706
KevinKMB <kevin.maldonado.est@tecazuay.edu.ec>
2023-05-11 03:13:41 -0500

    Perfiles y Dashboard

--- src/app/modules/director-carrera/director-dashboard/director-dashboard.module.ts
+++ src/app/modules/director-carrera/director-dashboard/director-dashboard.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DirectorDashboardRoutingModule } from './director-dashboard-routing.module';
 import { DirectorDashboardComponent } from './pages/director-dashboard/director-dashboard.component';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { DirectorDashboardComponent } from './pages/director-dashboard/director-
   ],
   imports: [
     CommonModule,
-    DirectorDashboardRoutingModule
+    DirectorDashboardRoutingModule,
+    MatCardModule
   ],
   exports:[
     DirectorDashboardComponent
