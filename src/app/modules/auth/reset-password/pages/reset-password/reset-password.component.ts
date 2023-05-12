2f64c13f7f71bf4723db57eb5b1b76f05cde3815
Chimbo-K18 <kevin.chimbo.est@tecazuay.edu.ec>
2023-05-12 01:58:53 -0500

    Aun falta

--- src/app/modules/auth/reset-password/pages/reset-password/reset-password.component.ts
+++ src/app/modules/auth/reset-password/pages/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  cedula!: string;
+  newPassword!: string;
+  message!: string;
+
+  constructor(private userService: UserService) { }
+
+
 
   ngOnInit(): void {
   }
+
+  onResetPassword() {
+    this.userService.resetPassword(this.cedula, this.newPassword).subscribe(
+      () => {
+        this.message = "Contraseña restablecida exitosamente.";
+      },
+      (error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.message = "No se encontró ningún usuario con el número de cédula proporcionado.";
+        } else {
+          this.message = "Error al restablecer la contraseña.";
+        }
+      }
+    );
+  }
+
 
 }
