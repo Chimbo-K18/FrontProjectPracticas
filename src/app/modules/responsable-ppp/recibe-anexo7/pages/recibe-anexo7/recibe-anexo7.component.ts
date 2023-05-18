512111cd78bbcd7af9b349a3e8a3dc650aa5dcdf
David <100893416+davicho896@users.noreply.github.com>
2023-05-18 12:35:33 -0500

    Faltas de ortografia al igual que crear nuevas vistas
    Faltas de ortografia al igual que crear nuevas vistas

--- src/app/modules/responsable-ppp/recibe-anexo7/pages/recibe-anexo7/recibe-anexo7.component.ts
+++ src/app/modules/responsable-ppp/recibe-anexo7/pages/recibe-anexo7/recibe-anexo7.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import { Responsable_PPPService } from 'src/app/services/responsable_ppp.service';
+import { PracticaService } from 'src/app/services/practica.service';
+import { Practica } from 'src/app/models/practica';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-recibe-anexo7',
   templateUrl: './recibe-anexo7.component.html',
   styleUrls: ['./recibe-anexo7.component.css']
 })
-export class RecibeAnexo7Component implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+export class RecibeAnexo7Component implements AfterViewInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'carrera','descargar'];
+  dataSource = new MatTableDataSource<Practica>([]);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+  constructor(private responsableppservice: Responsable_PPPService, private practicaservice: PracticaService){
+    
+  }
+  ngOnInit(): void {
+
+    this.listarAnexos();
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  Ce:any;
+  carreradata:any;
+  listaconvocatoria: any[] = [];
+  listarAnexos() {
+    this.Ce = localStorage.getItem("idusuario");
+    console.log(this.Ce);
+    this.responsableppservice.getBuscarcedula(this.Ce).subscribe(datausu => {
+      console.log(datausu);
+    this.carreradata = datausu.carrera;
+        this.practicaservice.listarPorAnexo1Recibe(this.carreradata).subscribe(dataconvo => {
+          console.log(dataconvo);
+          this.listaconvocatoria = dataconvo;
+          this.dataSource.data = this.listaconvocatoria;
+
+        });
+      });
+
+  }
+  anexo1generado:any;
+  descargarPDF(idAnexo1 :any) {
+    this.anexo1generado = idAnexo1; // obtén el ID de la solicitud
+    const url = `http://localhost:8080/api/jasperReport/anexo1/${this.anexo1generado}`;
+    window.open(url, '_blank');
+  }
+
+ 
 
 }
--- src/app/modules/responsable-ppp/recibe-anexo8/recibe-anexo8.module.ts
+++ src/app/modules/responsable-ppp/recibe-anexo8/recibe-anexo8.module.ts
@@ -5,12 +5,38 @@ import { RecibeAnexo8RoutingModule } from './recibe-anexo8-routing.module';
 import { RecibeAnexo8Component } from './pages/recibe-anexo8/recibe-anexo8.component';
 
 
+//Importaciones clave
+
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 @NgModule({
   declarations: [
     RecibeAnexo8Component
   ],
   imports: [
     CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
     RecibeAnexo8RoutingModule
   ],
   exports:[
