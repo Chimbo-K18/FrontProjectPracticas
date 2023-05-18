874868da41a68301c1bac4549df74c6240aa11af
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2023-05-17 22:59:58 -0500

    listado convocatorias

--- src/app/modules/responsable-ppp/lista-convocatorias/pages/lista-convocatorias/lista-convocatorias.component.ts
+++ src/app/modules/responsable-ppp/lista-convocatorias/pages/lista-convocatorias/lista-convocatorias.component.ts
@@ -8,6 +8,8 @@ import { MatSort } from '@angular/material/sort';
 import { ConvocatoriasService } from 'src/app/services/convocatorias.service';
 import { Convocatorias } from 'src/app/models/convocatorias';
 import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
+import { log } from 'console';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -35,7 +37,7 @@ export class ListaConvocatoriasComponent implements OnInit {
   convocatorias: Convocatorias | any;
   matcher = new MyErrorStateMatcher();
 
-  displayedColumns: string[] = ['nombreConvocatoria', 'fechaPublicacion', 'fechaExpiracion', 'estadoConvocatoria', 'carrera'];
+  displayedColumns: string[] = ['convocatoria', 'FechaPublicacion', 'FechaExpiracion', 'estado', 'carrera','fechaAprobacion'];
   dataSource = new MatTableDataSource<any>([]);
   apiResponse: any = [];
   listaConvocatoria: Convocatorias[] = [];
@@ -45,21 +47,42 @@ export class ListaConvocatoriasComponent implements OnInit {
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private convocatoriaService: ConvocatoriasService,
-    private userservice: UserService) { }
-  datos: { nombreconvocatoria: string, fechapublicacion: string, fechaexpiracion: string, estadoConvocatoria: boolean, carrera: string }[] = [];
+    private userservice: UserService,
+    private http: HttpClient) { }
+  datos: { convocatoria: string, FechaPublicacion: string, FechaExpiracion: string, estado: boolean, carrera: string ,fechaAprobacion:string}[] = [];
 
 
   ngOnInit(): void {
-    this.obtenerConvocatorias();
+   this.getConvocatorias();
   }
 
-  //obtener convocatorias
+   //obtener convocatorias
   //obtener convocatorias
   storageUser: any;
   varcarrera: any;
   usuariocarrera: any;
   estadoTexto: any;
 
+  getConvocatorias(){
+    this.http.get('http://localhost:8080/api/convocatorias/datos').subscribe({
+      next: (response: any) => {
+        console.log(response.estado);
+        this.datos = response;
+        this.dataSource = new MatTableDataSource<any>(this.datos);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.estadoTexto = response.estado ? "Aprobada" : "Pendiente";
+        
+        
+      },
+      error: (err) => {
+        alert("Error while fetching the records")
+      }
+    }); 
+  }
+
+ 
+  /*
   obtenerConvocatorias() {
     this.storageUser = localStorage.getItem("idusuario");
     console.log("id usuario " + this.storageUser)
@@ -84,7 +107,7 @@ export class ListaConvocatoriasComponent implements OnInit {
     });
 
   }
-
+*/
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
