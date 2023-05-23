820a7ce59d34edd23064b528dfabab34984006dd
ChristianBarb01 <christianp.barbecho.est@tecazuay.edu.ec>
2023-05-23 17:12:46 -0500

    probar

--- src/app/modules/director-carrera/lista-sol-convocatorias/pages/lista-sol-convocatorias/lista-sol-convocatorias.component.ts
+++ src/app/modules/director-carrera/lista-sol-convocatorias/pages/lista-sol-convocatorias/lista-sol-convocatorias.component.ts
@@ -3,11 +3,8 @@ import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import { ConvocatoriasService } from 'src/app/services/convocatorias.service';
-import { SolicitudConvocatoriasService } from 'src/app/services/solicitudconvocatoria.service';
-import { SolicitudPracticas } from 'src/app/models/solicitudpracticas';
 import { OnInit } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
-
 @Component({
   selector: 'app-lista-sol-convocatorias',
   templateUrl: './lista-sol-convocatorias.component.html',
@@ -25,8 +22,7 @@ export class ListaSolConvocatoriasComponent implements OnInit {
   }
   constructor(
     private convocatoriaService: ConvocatoriasService, 
-    private UserService:UserService,
-    private solicitudconvocatoriaservice: SolicitudConvocatoriasService) {
+    private UserService:UserService) {
     this.obtenersolicitudes();
   }
 
@@ -37,19 +33,23 @@ export class ListaSolConvocatoriasComponent implements OnInit {
   idusuarious: any;
   datadirector: any;
   listaconvocatorias:any;
-  //obtener convocatorias
+
   obtenersolicitudes() {
     this.idusuarious = localStorage.getItem("idusuario");
     this.UserService.getuscedula(this.idusuarious).subscribe(datausu => {
-    this.datadirector = datausu.carrera;
-    console.log(this.datadirector);
-    this.convocatoriaService.listarConvocatoriasPorCarrera(this.datadirector).subscribe((data) => {
+    this.convocatoriaService.listarConvocatoriasPorCarrera(datausu.carrera).subscribe({
+      next: (data) => {
       this.listaconvocatorias = data;
+      console.log("estas es la data de convocatoria")
             console.log(data); 
             this.dataSource.data = this.listaconvocatorias
             this.dataSource = new MatTableDataSource<any>(this.listaconvocatorias);
             this.dataSource.paginator = this.paginator;
             this.dataSource.sort = this.sort;
+          }, 
+          error: (err) => {
+        console.log("error xd")
+          }
           });
     });
    
