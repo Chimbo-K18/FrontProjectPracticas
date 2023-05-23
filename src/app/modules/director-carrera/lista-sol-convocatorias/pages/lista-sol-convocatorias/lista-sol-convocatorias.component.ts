9aebb34c02f400ec421760cbe33fadd51aa7ec51
ChristianBarb01 <christianp.barbecho.est@tecazuay.edu.ec>
2023-05-23 12:41:41 -0500

    listado

--- src/app/modules/director-carrera/lista-sol-convocatorias/pages/lista-sol-convocatorias/lista-sol-convocatorias.component.ts
+++ src/app/modules/director-carrera/lista-sol-convocatorias/pages/lista-sol-convocatorias/lista-sol-convocatorias.component.ts
@@ -2,41 +2,24 @@ import { UserService } from './../../../../../services/user.service';
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
-import { Convocatorias } from 'src/app/models/convocatorias';
 import { ConvocatoriasService } from 'src/app/services/convocatorias.service';
 import { SolicitudConvocatoriasService } from 'src/app/services/solicitudconvocatoria.service';
-import { SolicitudConvocatoria } from 'src/app/models/solicitudconvocatoria';
-
-
-
+import { SolicitudPracticas } from 'src/app/models/solicitudpracticas';
+import { OnInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-lista-sol-convocatorias',
   templateUrl: './lista-sol-convocatorias.component.html',
   styleUrls: ['./lista-sol-convocatorias.component.css']
 })
-export class ListaSolConvocatoriasComponent  {
-  convocatorias: Convocatorias | any;
-  mivariable !: any;
-  //TABLA
-  //TABLA convocatorias
-  displayedColumns: string[] = [
-    'nombreconvocatoria',
-    'fechapublicacion',
-    'fechaexpiracion',
-    'estadoConvocatoria',
-  ];
-  listaConvocatoria: Convocatorias[] = [];
-  solicitudconvocatoria: SolicitudConvocatoria = new SolicitudConvocatoria();
-  loading: boolean = true;
-  dataSource = new MatTableDataSource<Convocatorias>([]);
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+export class ListaSolConvocatoriasComponent implements OnInit {
+  displayedColumns: string[] = ['nombre', 'descripcion', 'fechaE', 'fechaA','estado'];
+  dataSource = new MatTableDataSource<any>([]);
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-
+  @ViewChild(MatSort) sort!: MatSort;
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
@@ -44,48 +27,40 @@ export class ListaSolConvocatoriasComponent  {
     private convocatoriaService: ConvocatoriasService, 
     private UserService:UserService,
     private solicitudconvocatoriaservice: SolicitudConvocatoriasService) {
-    this.obtenerConvocatorias();
+    this.obtenersolicitudes();
   }
 
   ngOnInit(): void {
 
-    this.listaConvocatorias();
   }
-  listaConvocatorias() {
-    this.convocatoriaService.getConvocatorias().subscribe(
-      (res) => {
-        this.convocatorias = res;
-        console.log(this.convocatorias);
-      }
-    );
-  }
- 
   listassolicitudes: any[] = [];
   idusuarious: any;
   datadirector: any;
+  listaconvocatorias:any;
   //obtener convocatorias
-  obtenerConvocatorias() {
+  obtenersolicitudes() {
     this.idusuarious = localStorage.getItem("idusuario");
     this.UserService.getuscedula(this.idusuarious).subscribe(datausu => {
     this.datadirector = datausu.carrera;
     console.log(this.datadirector);
     this.convocatoriaService.listarConvocatoriasPorCarrera(this.datadirector).subscribe((data) => {
-      this.listaConvocatoria = data.map((result) => {
-        let convo = new Convocatorias();
-        convo.idConvocatorias = result.idConvocatorias;
-        convo.nombreConvocatoria = result.nombreConvocatoria;
-        convo.fechaPublicacion = result.fechaPublicacion
-        convo.fechaExpiracion = result.fechaExpiracion;
-        convo.documentoConvatoria = result.documentoConvatoria;
-        return convo;
-      });
-      this.dataSource.data = this.listaConvocatoria;
-      console.log(this.listaConvocatoria);
-      this.loading = false;
-
-    });
+      this.listaconvocatorias = data;
+            console.log(data); 
+            this.dataSource.data = this.listaconvocatorias
+            this.dataSource = new MatTableDataSource<any>(this.listaconvocatorias);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+          });
     });
    
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
